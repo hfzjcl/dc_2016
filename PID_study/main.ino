3bab9d8b962ff6943e47794bb5c64c081e9a746d
timfaner <grngrngrngrn@163.com>
2016-08-26 22:34:53 +0800

    完善合作分享模式

--- PID_study/main.ino
+++ PID_study/main.ino
@@ -1,6 +1,6 @@
-void initialization(struct moto, struct moto, int, int, int, int);
-
+// #include "PID.h"//声明函数
 int motor_work(struct motor moto, int pwm);
+//中断程序
 void doencoder_l() {
   if (digitalRead(en_l_pin_a) == digitalRead(en_l_pin_b))
     en_l_pos_a++;
@@ -13,13 +13,36 @@ void doencoder_r() {
   else
     en_r_pos_a--;
 }
+
 void setup() {
-  initialization(motor_l, motor_r, en_l_pin_a, en_l_pin_b, en_r_pin_a, en_r_pin_b);
-  attachInterrupt(digitalPinToInterrupt(en_l_pin_a), doencoder_l , CHANGE);
-  attachInterrupt(digitalPinToInterrupt(en_r_pin_a), doencoder_r , CHANGE);
+  motor motor_l = {'l', motor_l_in1, motor_l_in2, motor_l_pwm};
+  motor motor_r = {'r', motor_r_in1, motor_r_in2, motor_r_pwm};
+
+  Serial.begin(9600);
+  pinMode(motor_l.in1, OUTPUT);
+  pinMode(motor_l.in2, OUTPUT);
+  pinMode(motor_r.in1, OUTPUT);
+  pinMode(motor_r.in2, OUTPUT);
+  pinMode(en_l_pin_a, INPUT);
+  pinMode(en_r_pin_a, INPUT);
+  pinMode(en_l_pin_b, INPUT);
+  pinMode(en_r_pin_b, INPUT);
+  attachInterrupt(digitalPinToInterrupt(en_l_pin_a), doencoder_l , RISING);
+  attachInterrupt(digitalPinToInterrupt(en_r_pin_a), doencoder_r , RISING);
 }
+int Target = 195 + 780;
+int i = 0;
+int pwm;
 void loop() {
-  char pos = 'l';
-  motor_work(motor_l, 255);
+  Serial.print(en_l_pos_a);
+  Serial.print(",");
+  Serial.println(en_r_pos_a);
+  pwm = Position_PID(en_l_pos_a, Target);
+  motor_work(motor_l,pwm);
+  delay(10);
+
+
+
+
 }
 
--- PID_study/motor.ino
+++ PID_study/motor.ino
@@ -1,14 +1,4 @@
 
-void initialization(struct motor moto_l,struct motor moto_r, int en_l_pin_a, int en_r_pin_a, int en_l_pin_b, int en_r_pin_b) { //放setup里
-  pinMode(moto_l.in1, OUTPUT);
-  pinMode(moto_l.in2, OUTPUT);
-  pinMode(moto_r.in1, OUTPUT);
-  pinMode(moto_r.in2, OUTPUT);
-  pinMode(en_l_pin_a, INPUT);
-  pinMode(en_r_pin_a, INPUT);
-  pinMode(en_l_pin_b, INPUT);
-  pinMode(en_r_pin_b, INPUT);
-}
 
 int motor_work(struct motor moto, int pwm) { //运转函数
   if (moto.Name == 'l') {
